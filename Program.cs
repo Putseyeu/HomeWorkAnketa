8bea4931cb0d7b81ca277dc05b37a96f59098340
Putseyeu <putseyeu.a.d@gmail.com>
2021-10-11 21:58:38 +0200

    step1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkAnketa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userSurname;
+            float userHeight;
+            float userAge;
+            string userСitiesResidence;
+            string userProfession;
+
+            Console.Write("Как вас зовут?:");
+            userName = Console.ReadLine();
+
+            Console.Write("Какая ваша фамилия ?:");
+            userSurname = Console.ReadLine();
+
+            Console.Write("Сколько  вам полных лет?:");
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Какой ваш рост?:");
+            userHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("В каком городе вы проживаете?:");
+            userСitiesResidence = Console.ReadLine();
+
+            Console.Write("Какая ваша профессия?:");
+            userProfession = Console.ReadLine();
+
+
+            Console.WriteLine($"Ваше полное имя {userName} {userSurname}, ваш возвраст  {userAge} год" +
+                $" и ваш рост {userHeight}, город в котором вы проживаете {userСitiesResidence} и ваша профессия {userProfession}");
+        }
+    }
+}
